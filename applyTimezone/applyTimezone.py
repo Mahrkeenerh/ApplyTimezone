669c9ac0a894f0c502b2989843d61f4ab8845c14
Mahrkeenerh <samuelbuban@gmail.com>
2021-03-11 21:58:34 +0100

    Timezone not required

--- applyTimezone/applyTimezone.py
+++ applyTimezone/applyTimezone.py
@@ -1,29 +1,49 @@
 from dateutil.parser import parse
 
 
-def dt2str(datetime_with_timezone):
+def dt2str(input_datetime):
 
-    out = datetime_with_timezone - datetime_with_timezone.utcoffset()
+    offset = input_datetime.utcoffset()
+
+    if offset == None:
+        return input_datetime
+
+    out = input_datetime - offset
 
     return out.strftime("%Y-%m-%d %H:%M:%S.%f")
 
 
-def dt2dt(datetime_with_timezone):
+def dt2dt(input_datetime):
 
-    out = datetime_with_timezone - datetime_with_timezone.utcoffset()
+    offset = input_datetime.utcoffset()
+
+    if offset == None:
+        return input_datetime
+
+    out = input_datetime - input_datetime.utcoffset()
 
     return out.replace(tzinfo=None)
 
 
-def str2str(datetime_string_with_timezone):
+def str2str(input_datetime):
 
-    out = parse(datetime_string_with_timezone) - parse(datetime_string_with_timezone).utcoffset()
+    offset = parse(input_datetime).utcoffset()
+
+    if offset == None:
+        return input_datetime
+
+    out = parse(input_datetime) - offset
 
     return out.strftime("%Y-%m-%d %H:%M:%S.%f")
 
 
-def str2dt(datetime_string_with_timezone):
+def str2dt(input_datetime):
 
-    out = parse(datetime_string_with_timezone) - parse(datetime_string_with_timezone).utcoffset()
+    offset = parse(input_datetime).utcoffset()
+
+    if offset == None:
+        return input_datetime
+
+    out = parse(input_datetime) - offset
 
     return out.replace(tzinfo=None)
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email='samuelbuban@gmail.com',
     packages=['applyTimezone'],
     install_requires=['python-dateutil'],
-    version='2.0',
+    version='2.1',
     license='MIT',
     description='A package to convert to UTC time',
     long_description=open('README.md').read()
